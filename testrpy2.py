8515dbdcc07ff4e740cabf8859e026e71d269ef6
Sam Cohen <samcohen96@gmail.com>
2021-02-04 20:35:09 -0500

    Updated dataframe

--- testrpy2.py
+++ testrpy2.py
@@ -2,7 +2,3 @@ import rpy2.robjects.lib.ggplot2 as ggplot2
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
 import csv
-import pandas as pd
-appleActifityDF = pd.read_csv("Apple Music Play Activity12.2.20.csv")
-
-adfa
