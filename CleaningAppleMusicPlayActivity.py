2d0398d566ceecbaa95a71cced93f6993697315f
Sam Cohen <samcohen96@gmail.com>
2021-03-04 23:38:59 -0500

    about to make line plot

--- CleaningAppleMusicPlayActivity.py
+++ CleaningAppleMusicPlayActivity.py
@@ -51,7 +51,7 @@ index = 0
 # Local Time
 appleActifityDF["LocalTimeSeconds"] = appleActifityDF["UTCTimeSeconds"] + appleActifityDF["UTC Offset In Seconds"]
 
-print(appleActifityDF.head(59))
+
 def convertLocalSecondsToLocalTime(localTimeSeconds):
     global index
     index = index + 1
@@ -71,12 +71,12 @@ appleActifityDF["LocalTimeFloorHours"] = appleActifityDF["LocalTime"].apply(
     lambda a: int(re.search("[^:]*", a).group(0)))
 
 # histogram of hours most listend during
-appleActifityDF["LocalTimeFloorHours"].value_counts().plot(kind='bar')
-plt.show()
+#appleActifityDF["LocalTimeFloorHours"].value_counts().plot(kind='bar')
+#plt.show()
 
 # Removing songs played on the sonos?
 # print(appleActifityDF["Build Version"].value_counts())
-# appleActifityDF = appleActifityDF[~appleActifityDF["Build Version"].str.contains("sonos", na=False)]
+appleActifityDF = appleActifityDF[~appleActifityDF["Build Version"].str.contains("sonos", na=False)]
 
 # creating a play percentage column
 appleActifityDF["ListenedPercent"] = appleActifityDF["Play Duration Milliseconds"] / appleActifityDF[
@@ -97,9 +97,15 @@ appleActifityDF = appleActifityDF[
                                                          appleActifityDF["ListenedDurationInMinutes"].quantile(.99))]
 
 appleActifityDF = appleActifityDF.drop(["Apple Id Number", "Apple Music Subscription", "Client IP Address", "Content Specific Type",
-"Device Identifier", "Event End Timestamp", "Event Reason Hint Type", "Event Received Timestamp", "Item Type", "Metrics Bucket Id", "Metrics Client Id", "Original Title", "Store Country Name", "UTC Offset In Seconds", "End Position In Milliseconds", "Event End Timestamp"], axis=1)
+"Device Identifier", "Event End Timestamp", "Event Reason Hint Type", "Event Received Timestamp", "Item Type", "Metrics Bucket Id", "Metrics Client Id", "Original Title", "Store Country Name", "UTC Offset In Seconds", "End Position In Milliseconds", "Event End Timestamp", "Event Start Timestamp", ], axis=1)
+
+# Next steps returning chart of most frequently listened to artists
+#Top 20 most frequently played artists
+top20Artists = appleActifityDF["Artist Name"].value_counts()[:20].index.tolist()
+
+#df with only the top 20 artists
+appleActifityDFTop20 = appleActifityDF[pd.DataFrame(appleActifityDF["Artist Name"].tolist()).isin(top20Artists).any(1).values]
 
 
-print(appleActifityDF["ListenedDurationInMinutes"].std())
-print(appleActifityDF.head(1))
-
+top20ByMonthYear = appleActifityDFTop20.groupby(["Artist Name", 'Year', "Month"])["Artist Name"].count()
+print(top20ByMonthYear.tail(65))
